cd12be92a93c1fd8da25190639763946cecb8245
Rezza Mochamad <rezzamuhammad12@gmail.com>
2019-10-02 17:31:01 +0700

    Menambahkan property dan method pada kelas produk seperti judul,penulis,penerbit, dan harga

--- produk.php
+++ produk.php
@@ -1,12 +1,38 @@
 <?php 
 
-// Jualan Produk
-// Komik
-// Game
-
 class Produk {
+	public $judul = "judul", 
+		   $penulis = "penulis",
+		   $penerbit = "penerbit",
+		   $harga = 0;
 
+	public function getLabel() {
+		return "$this->penulis, $this->penerbit";
+	}
+	     
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "One Piece";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Shikamaru";
+$produk3->penerbit = "Uciha";
+$produk3->harga = 50000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
